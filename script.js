5c7af90b98acb3af6e949d51be7411802e134752
FIAN.F2 <alfianabu87@gmail.com>
2025-08-16 10:45:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ const quote = document.getElementById('quote');
 
 const quotes = [
  "Percayalah pada dirimu sendiri.",
-  "Setiap hari adalah awal yang baru.",
-  "Hal baik membutuhkan waktu.",
-  "Kebahagiaan berawal dari hati.",
-  "Keberanian bukanlah ketiadaan rasa takut.",
-  "Petualangan menanti mereka yang berani.",
-  "Langkah kecil bisa membawa perubahan besar.",
-  "Keberuntungan berpihak pada yang berusaha.",
-  "Kesabaran adalah kunci dari segalanya.",
-  "Masa depanmu ditentukan oleh apa yang kau lakukan hari ini.",
-  "YNTKTS.",
-  "Hdp Jkw.",
-  "Jadiginile."
+"Setiap hari adalah awal yang baru.",
+"Hal baik membutuhkan waktu.",
+"Kebahagiaan berawal dari hati.",
+"Masa depanmu ditentukan oleh hari ini.",  
+"Gas terus, jangan banyak alasan!",
+"Kalau capek istirahat, jangan menyerah.",
+"Mimpi besar dimulai dari langkah kecil.",
+"Belajar itu berat, tapi nggak belajar lebih berat.",
+"Sukses itu hasil dari konsistensi kecil tiap hari.",
+"Nasi padang adalah solusi dari semua masalah.",
+"Tidur sebentar = bangun besok pagi.",
+"WiFi kenceng lebih bikin bahagia daripada mantan.",
+"Hdp Jkw.",
+"“JadiGiniLe.",
+"YNTKTS.",
+"My Favorite Musik Gue.",
+"Mikir keras malah lapar.",
+"Air galon lebih setia daripada pacar.",
+"Santuy aja bro, dunia nggak kemana-mana.",
+"Gaskeun sebelum di-gas orang lain.",
+"Nggak usah panik, semua masih bisa di-restart.",
+"Rebahan itu hak asasi manusia.",
+"Jangan lupa bahagia, biar nggak jadi npc."
+
 ];
 
 sphere.addEventListener('click', () => {
